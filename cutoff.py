cd7e09d6d5f45476133d14c574788abe459f34cb
Lucas Treffenstädt <lucas.treffenstaedt@tngtech.com>
2021-08-21 16:34:39 +0200

    feat: use a cutoff to prevent negative density

---
+++ cutoff.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class Cutoff:
+    def __init__(self, cutoff: float):
+        self._cutoff = cutoff
+
+    def cutoff(self, array: np.array):
+        array[array < self._cutoff] = self._cutoff
+        return array
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import sys
 from analysis import Analysis
 from diffusion import Diffusion
+from cutoff import Cutoff
 
 num_bins = 4096
 dr = 2**-7
@@ -9,17 +10,19 @@ small_steps = 10**6
 big_steps = 100
 simulation_time = 1.0
 
+
 if __name__ == "__main__":
     analysis = Analysis(dr, num_bins)
     diffusion = Diffusion(analysis, 1.0/small_steps)
+    cutoff = lambda a: Cutoff(1e-70).cutoff(a)
 
-    rho0 = analysis.delta() + 1e-70
-    rho = rho0
+    rho0 = analysis.delta()
+    rho = cutoff(rho0)
     for t in range(big_steps+1):
         norm = analysis.integrate(rho)
         np.savetxt(sys.stdout.buffer, rho,
                    header='# t = {}\n# norm = {:.30f}'.format(t/big_steps, norm), footer='\n', comments='')
         print('big step: {}'.format(t), file=sys.stderr)
         for tt in range(small_steps//big_steps):
-            rho = diffusion.step(rho)
+            rho = cutoff(diffusion.step(rho))
 
