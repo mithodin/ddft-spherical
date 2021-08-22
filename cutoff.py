f1c41cb915cdba7180930856de0926d7c8542312
Lucas Treffenstädt <lucas.treffenstaedt@tngtech.com>
2021-08-22 09:27:40 +0200

    test: add tests for cutoff

--- cutoff.py
+++ cutoff.py
@@ -7,3 +7,14 @@ class Cutoff:
     def cutoff(self, array: np.array):
         array[array < self._cutoff] = self._cutoff
         return array
+
+
+def test_cutoff():
+    cutoff = Cutoff(0.5)
+    arr = -np.random.rand(128)
+    assert np.all(cutoff.cutoff(arr) == 0.5)
+
+    arr = np.array([1, 2, 3, 0, 0, 2, -1], dtype=np.float)
+    assert np.all(cutoff.cutoff(arr) == np.array([1, 2, 3, 0.5, 0.5, 2, 0.5]))
+
+
