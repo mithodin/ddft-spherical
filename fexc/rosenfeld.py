9c7daa9783d453cbd3a3032234880d15ca96dba7
Lucas Treffenstädt <lucas.treffenstaedt@tngtech.com>
2021-08-29 09:55:22 +0200

    feat: use sympy.lambdify to avoid eval

--- fexc/rosenfeld.py
+++ fexc/rosenfeld.py
@@ -20,28 +20,17 @@ class Rosenfeld(Fexc):
         n1v = n2v / (4*sy.pi*R)
         n0 = n2 / (4*sy.pi*R**2)
         phi = -n0*sy.log(1-n3)+(n1*n2-n1v*n2v)/(1-n3) + n2**3*(1-sy.Abs(n2v/n2))**3/(24*sy.pi*(1-n3)**2)
-        self._phi = "{}".format(phi)
+        self._phi = sy.lambdify([n2, n3, n2v, R], phi)
         self._dphi = {
-            WD.N2: "{}".format(sy.simplify(phi.diff(n2))),
-            WD.N3: "{}".format(sy.simplify(phi.diff(n3))),
-            WD.N2V: "{}".format(sy.simplify(phi.diff(n2v)))
-        }
-
-    def _get_mappings(self, n2, n3, n2v, radius_sphere):
-        return {
-            "log": np.log,
-            "Abs": np.abs,
-            "pi": np.pi,
-            "n2": n2,
-            "n2v": n2v,
-            "n3": n3,
-            "R": radius_sphere
+            WD.N2: sy.lambdify([n2, n3, n2v, R], phi.diff(n2)),
+            WD.N3: sy.lambdify([n2, n3, n2v, R], phi.diff(n3)),
+            WD.N2V: sy.lambdify([n2, n3, n2v, R], phi.diff(n2v)),
         }
 
     def fexc(self, rho: (np.array, np.array)) -> float:
         rho_tot = rho[0] + rho[1]
         n2, n3, n2v = self._wd.calc_densities([WD.N2, WD.N3, WD.N2V], rho_tot)
-        phi = eval(self._phi, dict(), self._get_mappings(n2, n3, n2v, self._R))
+        phi = self._phi(n2, n3, n2v, self._R)
         return self._ana.integrate(phi)
 
     def d_fexc_d_rho(self, rho: (np.array, np.array)) -> (np.array, np.array):
@@ -55,11 +44,11 @@ def test_rf_expression():
     ana = Analysis(dr, n)
     rf = Rosenfeld(ana, None)
 
-    n2 = 0.5
-    n3 = 0.5
-    n2v = 0.3
+    n2 = np.array([0.5, 0.1, 0.2])
+    n3 = np.array([0.1, 0.3, 0.8])
+    n2v = np.array([0.0, 0.3, 0.5])
     n0 = n2 / np.pi
     n1 = n2 / (2*np.pi)
     n1v = n2v / (2*np.pi)
 
-    assert eval(rf._phi, dict(), rf._get_mappings(n2, n3, n2v, 0.5)) == approx(-n0*np.log(1-n3)+(n1*n2-n1v*n2v)/(1-n3) + n2**3*(1-np.abs(n2v/n2))**3/(24*np.pi*(1-n3)**2))
+    assert rf._phi(n2, n3, n2v, 0.5) == approx(-n0*np.log(1-n3)+(n1*n2-n1v*n2v)/(1-n3) + n2**3*(1-np.abs(n2v/n2))**3/(24*np.pi*(1-n3)**2))
