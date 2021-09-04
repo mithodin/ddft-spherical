318a0fc352ce11d592396b550f3a3223d0768ebf
Lucas Treffenstädt <lucas.treffenstaedt@tngtech.com>
2021-09-04 15:13:07 +0200

    fix: return numbers as double

--- test/test_cutoff.py
+++ test/test_cutoff.py
@@ -8,5 +8,5 @@ def test_cutoff():
     arr = -np.random.rand(128)
     assert np.all(cutoff.cutoff(arr) == 0.5)
 
-    arr = np.array([1, 2, 3, 0, 0, 2, -1], dtype=np.float)
+    arr = np.array([1, 2, 3, 0, 0, 2, -1], dtype=np.double)
     assert np.all(cutoff.cutoff(arr) == np.array([1, 2, 3, 0.5, 0.5, 2, 0.5]))
