c71cbcc34ff4431401e884ac1790f1b641f183ad
Lucas Treffenstädt <lucas.treffenstaedt@tngtech.com>
2021-09-14 08:39:54 +0200

    feat: calculate gradient with a sparse matrix

--- analysis.py
+++ analysis.py
@@ -12,14 +12,14 @@ class Analysis:
         n = self.n
         dr = self.dr
 
-        self.weights = (6 * np.arange(n) ** 2 + 1) * 2.0
-        self.weights[0] = 1
-        self.weights[-1] = (6 * (n - 1) ** 2 - 4 * (n - 1) + 1)
-        self.weights *= dr ** 3 * np.pi / 3
+        weights = (6 * np.arange(n, dtype=np.float64) ** 2 + 1) * 2.0
+        weights[0] = 1
+        weights[-1] = (6 * (n - 1) ** 2 - 4 * (n - 1) + 1)
+        weights *= dr ** 3 * np.pi / 3
+        self.weights = weights
 
-        weights = self.weights
-        div_op = np.zeros((n, n))
-        div_op[0, 1] = -weights[0]
+        div_op = np.zeros((n, n), dtype=np.float64)
+        div_op[0, 1] = -1./weights[0]
         div_op[-1, -2] = (n-2)**2/weights[-1]
         div_op[-1, -1] = -n**2/weights[-1]
         for k in range(1, n-1):
@@ -28,14 +28,19 @@ class Analysis:
         div_op *= -2*np.pi*dr**2
         self._div_op = sparse.csr_matrix(div_op)
 
+        grad_op = np.zeros((n, n), dtype=np.float64)
+        grad_op[0, 0:3] = [-3./2., 2., -1./2.]
+        grad_op[-1, n-3:n] = [1./2., -2., 3./2.]
+        for k in range(1, n-1):
+            grad_op[k, k-1:k+2] = [-1./2., 0, 1./2.]
+        grad_op /= dr
+        self._grad_op = sparse.csr_matrix(grad_op)
+
     def integrate(self, f: np.array):
         return np.sum(f * self.weights)
 
     def gradient(self, f: np.array):
-        res = (np.roll(f, -1) - np.roll(f, 1)) / 2.
-        res[0] = (-3 * f[0] + 4 * f[1] - f[2]) / 2.
-        res[-1] = (f[-3] - 4 * f[-2] + 3 * f[-1]) / 2.
-        return res/self.dr
+        return self._grad_op.dot(f)
 
     def delta(self):
         res = np.zeros(self.n)
