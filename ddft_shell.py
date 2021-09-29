878269f098daf9bbb52b8cc5ff0abd6a4e380030
Thomas Schindler <thomas.schindler@mayflower.de>
2021-09-29 22:05:53 +0200

    fix current

--- ddft_shell.py
+++ ddft_shell.py
@@ -18,24 +18,23 @@ class DDFTShell(DDFT):
         self._rho_shell_bulk_s = self._rho_bulk_s * 4. * np.pi * self._dr**2 * self.n**2
         self._rho_shell_bulk_d = self._rho_bulk_d * 4. * np.pi * self._dr**2 * self.n**2
         
-        self._radius = np.multiply(range(self.n),self._dr)
-        self._radius_minus = self._radius - self._dr
-        self._radius_plus  = self._radius + self._dr
+        self._radius_extended = np.arange(self.n + 1)
+        self._radius_extended[0] = -1
+        self._radius_extended *= self._dr
 
-        self._radius_minus[1] = -self._dr
-
-        self._normalized_shell_surfaces = np.power(range(self.n), 2)
-        self._normalized_shell_surfaces[0] = 1
-        self._normalized_shell_surfaces = self._normalized_shell_surfaces * 4. * np.pi * self._dr**2
+        self._normalized_shell_surfaces = 4. * np.pi * self._dr**2 * np.power(range(1, self.n), 2)
 
         self._rho_shell_s = self.to_shell_density(self._cutoff(rho0[0]).copy())
         self._rho_shell_d = self.to_shell_density(self._cutoff(rho0[1]).copy())
 
     def step(self, f_ext: (np.array, np.array) = None) -> (np.array, np.array, np.array, np.array):
         j_exc = self.j_exc()
+
+        #rho_s, j_s = self.step_s_d()
+
         j_s = j_exc[0]
         j_d = j_exc[1]
-
+        
         rho_s = self.to_volume_density(self._rho_shell_s)
         rho_d = self.to_volume_density(self._rho_shell_d)
 
@@ -45,7 +44,7 @@ class DDFTShell(DDFT):
         
         j_shell_s = self.to_shell_density(j_s)
         j_shell_d = self.to_shell_density(j_d)
-
+        
         rho_minus_s = np.concatenate(([rho_s[0]], rho_s[:-1]))
         rho_minus_d = np.concatenate(([rho_d[0]], rho_d[:-1]))
         rho_plus_s  = np.concatenate((rho_s[1:], [self._rho_bulk_s]))
@@ -53,28 +52,28 @@ class DDFTShell(DDFT):
 
         j_s += - self._D * (rho_plus_s - rho_minus_s) / (2 * self._dr)
         j_d += - self._D * (rho_plus_d - rho_minus_d) / (2 * self._dr)
+        
+        rho_shell_extended_s = np.concatenate(([0],self._rho_shell_s,[self._rho_shell_bulk_s]))
+        rho_shell_extended_d = np.concatenate(([0],self._rho_shell_d,[self._rho_shell_bulk_d]))
 
-        rho_shell_minus_s = np.concatenate(([0], self._rho_shell_s[:-1]))
-        rho_shell_minus_d = np.concatenate(([0], self._rho_shell_d[:-1]))
-        rho_shell_plus_s  = np.concatenate((self._rho_shell_s[1:], [self._rho_shell_bulk_s]))
-        rho_shell_plus_d  = np.concatenate((self._rho_shell_d[1:], [self._rho_shell_bulk_d]))
-
-        j_shell_minus_s = np.concatenate(([-j_shell_s[0]], j_shell_s[:-1]))
-        j_shell_minus_d = np.concatenate(([-j_shell_d[0]], j_shell_d[:-1]))
-        j_shell_plus_s  = np.concatenate((j_shell_s[1:], [0]))
-        j_shell_plus_d  = np.concatenate((j_shell_d[1:], [0]))
+        j_shell_extended_s = np.concatenate(([-j_shell_s[0]], j_shell_s, [j_shell_s[-1]]))
+        j_shell_extended_d = np.concatenate(([-j_shell_d[0]], j_shell_d, [j_shell_d[-1]]))
 
         D_rho_shell_s = - 2 / self._dr * self._rho_shell_s
         D_rho_shell_d = - 2 / self._dr * self._rho_shell_d
         
-        D_rho_shell_s += rho_shell_plus_s  / self._dr - np.divide(rho_shell_plus_s ,self._radius_plus ) - j_shell_plus_s  / (2 * self._D)
-        D_rho_shell_d += rho_shell_plus_d  / self._dr - np.divide(rho_shell_plus_d ,self._radius_plus ) - j_shell_plus_d  / (2 * self._D)
-        D_rho_shell_s += rho_shell_minus_s / self._dr + np.divide(rho_shell_minus_s,self._radius_minus) + j_shell_minus_s / (2 * self._D)
-        D_rho_shell_d += rho_shell_minus_d / self._dr + np.divide(rho_shell_minus_d,self._radius_minus) + j_shell_minus_d / (2 * self._D)
+        rho_shell_over_radius_extended_s = np.divide(rho_shell_extended_s, self._radius_extended)
+        rho_shell_over_radius_extended_d = np.divide(rho_shell_extended_d, self._radius_extended)
+
+        D_rho_shell_s += rho_shell_extended_s[2:]  / self._dr - rho_shell_over_radius_extended_s[2:]  - j_shell_extended_s[2:]  / (2 * self._D)
+        D_rho_shell_d += rho_shell_extended_d[2:]  / self._dr - rho_shell_over_radius_extended_d[2:]  - j_shell_extended_d[2:]  / (2 * self._D)
+        
+        D_rho_shell_s += rho_shell_extended_s[:-2] / self._dr + rho_shell_over_radius_extended_s[:-2] + j_shell_extended_s[:-2] / (2 * self._D)
+        D_rho_shell_d += rho_shell_extended_d[:-2] / self._dr + rho_shell_over_radius_extended_d[:-2] + j_shell_extended_d[:-2] / (2 * self._D)
         
         D_rho_shell_s *= self._D * self._dt / self._dr
         D_rho_shell_d *= self._D * self._dt / self._dr
-
+        
         self._rho_shell_s += D_rho_shell_s
         self._rho_shell_d += D_rho_shell_d
 
@@ -84,16 +83,16 @@ class DDFTShell(DDFT):
         return rho_s, rho_d, j_s, j_d
 
     def to_shell_density(self, volume_density):
-        return volume_density * self._normalized_shell_surfaces
+        return volume_density[1:] * self._normalized_shell_surfaces
 
     def to_volume_density(self,shell_density):
         result = shell_density / self._normalized_shell_surfaces
 
-        result[0] = self.extrapolate_to_zero_logarithmically(result[1], result[2])
-        if not np.isfinite(result[0]):
-            result[0] = result[1]
+        result0 = self.extrapolate_to_zero_logarithmically(result[0], result[1])
+        if not np.isfinite(result0):
+            result0 = result[0]
 
-        return result
+        return np.concatenate(([result0],result))
 
     def norms(self):
         return self._ana.integrate_shell(self._rho_shell_s), self._ana.integrate_shell(self._rho_shell_d)
--- test/test_ddft_shell.py
+++ test/test_ddft_shell.py
@@ -6,7 +6,7 @@ from diffusion import Diffusion
 from fexc.fexc import Fexc
 
 
-def test_constant_input():
+def test_constant_density():
     dr = 1
     n = 8
     rho_bulk = 1.
@@ -22,12 +22,15 @@ def test_constant_input():
 
     j_s_expected = np.zeros(n)
     j_d_expected = np.zeros(n)
+    rho_s_expected = rho_s_0
+    rho_d_expected = rho_d_0
+
     np.testing.assert_almost_equal(j_s_expected, j_s_1)
     np.testing.assert_almost_equal(j_d_expected, j_d_1)
-    np.testing.assert_almost_equal(rho_s_0, rho_s_1)
-    np.testing.assert_almost_equal(rho_d_0, rho_d_1)
+    np.testing.assert_almost_equal(rho_s_expected, rho_s_1)
+    np.testing.assert_almost_equal(rho_d_expected, rho_d_1)
 
-def test_delta_input():
+def test_delta_density():
     dr = 1
     n = 8
     rho_bulk = 1.
@@ -46,14 +49,42 @@ def test_delta_input():
 
     rho_s_1, rho_d_1, j_s_1, j_d_1 = ddft.step()
 
-    rho_s_expected = np.asarray([np.exp((4 * np.log(0.4) - np.log(0.025)) / 3), 0.4, 0.025, 0, 0, 0, 0, 0])
-    rho_d_expected = np.asarray([np.exp((4 * np.log(0.6) - np.log(0.975)) / 3), 0.6, 0.975, 1, 1, 1, 1, 1])
     j_s_expected = np.asarray([0, 0.25, 0.25, 0, 0, 0, 0, 0])
     j_d_expected = np.asarray([-0.0515749, -0.3015749, -0.25 , 0, 0, 0, 0, 0])
-    np.testing.assert_almost_equal(rho_s_expected, rho_s_1)
-    np.testing.assert_almost_equal(rho_d_expected, rho_d_1)
+    rho_s_expected = np.asarray([np.exp((4 * np.log(0.4) - np.log(0.025)) / 3), 0.4, 0.025, 0, 0, 0, 0, 0])
+    rho_d_expected = np.asarray([np.exp((4 * np.log(0.6) - np.log(0.975)) / 3), 0.6, 0.975, 1, 1, 1, 1, 1])
+    
     np.testing.assert_almost_equal(j_s_expected, j_s_1)
     np.testing.assert_almost_equal(j_d_expected, j_d_1)
+    np.testing.assert_almost_equal(rho_s_expected, rho_s_1)
+    np.testing.assert_almost_equal(rho_d_expected, rho_d_1)
+
+def test_constant_current():
+    dr = 1
+    n = 8
+    rho_bulk = 1.
+    dt = .1
+    rho_s_0 = np.zeros(n)
+    rho_d_0 = np.ones(n) * rho_bulk
+
+    ana = Analysis(dr, n)
+    f_exc = Fexc(ana)
+    ddft = DDFTShell(ana, dt, f_exc, (rho_s_0, rho_d_0), rho_bulk)
+
+    f_ext = 1 / (np.arange(n) * np.arange(n))
+    f_ext = np.stack((f_ext, f_ext))
+
+    rho_s_1, rho_d_1, j_s_1, j_d_1 = ddft.step(f_ext=f_ext)
+
+    j_s_expected = np.zeros(n)
+    j_d_expected = np.asarray([np.inf, 1., 0.25, 0.1111111, 0.0625 , 0.04 , 0.0277778, 0.0204082])
+    rho_s_expected = np.asarray([0, 0, 0, 0, 0, 0, 0, 0])
+    rho_d_expected = np.asarray([0.8689404, 0.9, 1, 1, 1, 1, 1, 1])
+
+    np.testing.assert_almost_equal(j_s_expected[1:], j_s_1[1:])
+    np.testing.assert_almost_equal(j_d_expected, j_d_1)
+    np.testing.assert_almost_equal(rho_s_expected, rho_s_1)
+    np.testing.assert_almost_equal(rho_d_expected, rho_d_1)
 
 def test_to_shell_and_back():
     dr = 1
