4d26a52326cc3ac1db65db632b7e20eb466eae8d
Lucas Treffenstädt <lucas.treffenstaedt@tngtech.com>
2021-10-24 17:26:48 +0200

    chore: clean up unneccessary data

--- legacy/free_energy.h
+++
@@ -1,5 +0,0 @@
-#ifdef __CUDACC__
-__global__ void dfts_free_energy(DFTS_t conf, bool grad, double *f);
-__global__ void dfts_f_ideal(DFTS_t conf, bool grad, double *f);
-__global__ void dfts_f_exc(DFTS_t conf, bool grad, double *f);
-#endif
