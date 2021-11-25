e0f03328b74012111667693e113d10a6a5bc9522
Lucas Treffenstädt <lucas.treffenstaedt@tngtech.com>
2021-11-25 08:19:56 +0100

    fix: read norm from logfile

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 mkdir -p out
 python ./main.py > out/diffusion.dat "$([ -z "$DDFT_CONFIG" ] && echo "config.jsonc" || echo "$DDFT_CONFIG")"
-grep "norm" out/diffusion.dat | cut -d '=' -f 2 > out/norm.dat
+grep "norm_self" out/diffusion.dat | cut -d '=' -f 2 > out/norm-self.dat
+grep "norm_dist" out/diffusion.dat | cut -d '=' -f 2 > out/norm-dist.dat
